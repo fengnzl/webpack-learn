d451f95c55bb43a3bcbfe960c530f0647e551ce2
recoveryMonster <fengsongting@gmail.com>
2020-11-16 23:25:04 +0800

    add --watch config option to watch file change

--- devServer/src/index.js
+++ devServer/src/index.js
@@ -1,9 +1,10 @@
 import './style.scss';
 function createEle () {
   const ele = document.createElement('div');
-  ele.innerHTML = 'hello webpack dev server';
+  ele.innerHTML = `hello webpack dev server it's lf`;
   ele.classList.add('txt-wrapper');
   return ele;
 }
+console.log('hello world adas')
 
 document.querySelector('#app').append(createEle());
