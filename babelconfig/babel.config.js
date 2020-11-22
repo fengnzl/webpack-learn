66416b882a5fe5ce39184cbb974f0453b79fe1e1
recoveryMonster <fengsongting@gmail.com>
2020-11-22 22:35:13 +0800

    babel runtime

--- babelconfig/babel.config.js
+++ babelconfig/babel.config.js
@@ -1,33 +1,44 @@
 module.exports = function (api) {
   api.cache(true);
-  const presets = [
-    [
-      "@babel/preset-env",
-      {
-        targets: { // 指定浏览器版本
-          edge: 17,
-          firefox: 60,
-          chrome: 67,
-          safari: 11.1,
-        },
-        modules: false, //用来指定模块化方式，支持 AMD、UMD、SystemJS、CommonJS 等。推荐将 modules 设置为 false，即交由 Webpack 来处理模块化，通过其 TreeShaking 特性将有效减少打包出来的 JS 文件大小
+  // const presets = [
+    // [
+    //   "@babel/preset-env",
+    //   {
+    //     targets: { // 指定浏览器版本
+    //       edge: 17,
+    //       firefox: 60,
+    //       chrome: 67,
+    //       safari: 11.1,
+    //     },
+    //     modules: false, //用来指定模块化方式，支持 AMD、UMD、SystemJS、CommonJS 等。推荐将 modules 设置为 false，即交由 Webpack 来处理模块化，通过其 TreeShaking 特性将有效减少打包出来的 JS 文件大小
         // useBuiltIns: "usage", // 只引入使用的polyfill
-      }
-    ]
-  ]
+    //   }
+    // ]
+  // ]
 
   const plugins = [
+    // [
+    //   "@babel/plugin-transform-regenerator",
+    //   {
+    //     asyncGenerators: false, // 异步生成器
+    //     generators: false, // 生成器
+    //     async: false, // 异步
+    //   }
+    // ]
     [
-      "@babel/plugin-transform-regenerator",
+      "@babel/plugin-transform-runtime",
       {
-        asyncGenerators: false, // 异步生成器
-        generators: false, // 生成器
-        async: false, // 异步
+        absoluteRuntime: false,
+        corejs: 3,
+        helpers: true,
+        regenerator: true,
+        useESModules: false,
+        version: "7.0.0-beta.0"
       }
     ]
   ]
   return {
-    presets,
+    // presets,
     plugins,
   }
 }
