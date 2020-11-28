549c62758a89608f4303fcf88a7f15698651ecdd
recoveryMonster <fengsongting@gmail.com>
2020-11-28 22:53:19 +0800

    add split chunks default configuration and modify chunks to all inorder to support async and sync chunks

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: paths.dist,
     filename: '[name].bundle.js',
-    chunkFilename: '[name].js'
+    // chunkFilename: '[name].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -22,11 +22,26 @@ module.exports = {
     new webpack.ProgressPlugin(),
     new CleanWebpackPlugin(),
   ],
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all'
-  //   }
-  // },
+  optimization: {
+    splitChunks: {
+      chunks: 'all', // 默认代码分割为async异步引入，all则是同步和异步都可以
+      minSize: 20000,
+      minRemainingSize: 0,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: false
+      }
+    }
+  },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 
-// console.log(_.join([1, 2, 3, 4], '***'));
+console.log(_.join([1, 2, 3, 4], '***'));
 
-async function createComponent () {
+
+// async function createComponent () {
   // return import(/* webpackChunkName: "lodash" */'lodash').then(({ default: _ }) => {
   //   const ele = document.createElement('div');
   //   ele.innerHTML = _.join(['a', 'b', 'c'], '**');
   //   return ele;
   // })
-  const element = document.createElement('div');
-  const { default: _ } = await import(/* webpackChunkName: "lodash" */'lodash');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
+//   const element = document.createElement('div');
+//   const { default: _ } = await import(/* webpackChunkName: "lodash" */'lodash');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   return element;
+// }
 
-createComponent().then(element => {
-  document.body.append(element);
-})
+// createComponent().then(element => {
+//   document.body.append(element);
+// })
