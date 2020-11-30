770656391b1b0fd0853f43907949f4c280bf840c
recoveryMonster <fengsongting@gmail.com>
2020-11-30 23:59:06 +0800

    learn pwa configuration initial comment

---
+++ pwa/config/webpack.common.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const paths = require('./paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const BaseConfig = {
+  entry: {
+    main: `${paths.src}/index.js`,
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'learn cacheing',
+      template: `${paths.src}/index.html`,
+      filename: 'index.html', // output file
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+  ],
+  optimization: {
+    usedExports: true,
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
+}
+module.exports = BaseConfig;
---
+++ pwa/config/webpack.dev.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const paths = require('./paths');
+const BaseConfig = require('./webpack.common');
+const { merge } = require('webpack-merge');
+
+const DevConfig = {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  output: {
+    path: paths.dist,
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: `${paths.src}/index.js`,
+        use: [
+          'babel-loader',
+          'imports-loader?wrapper=window',
+        ]
+
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          'style-loader',
+          { 
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.dist,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+}
+
+module.exports = merge(BaseConfig, DevConfig);
