ff6f5b270d67b40934dea8fdbe4707a79a006a44
recoveryMonster <fengsongting@gmail.com>
2020-12-05 18:06:54 +0800

    dev server proxy configuration

--- serverProxy/config/webpack.dev.js
+++ serverProxy/config/webpack.dev.js
@@ -12,6 +12,20 @@ const DevConfig = {
     contentBase: dist,
     open: true,
     compress: true,
+    proxy: {
+      '/react/api': {
+        target: 'http://www.dell-lee.com',
+        secure: false, // 可以访问https
+        changeOrigin: true, // 避免有的网站设置origin
+        pathRewrite: {
+          'header.json': 'demo.json', // 改变请求的数据，主要用于开发环境的借口变化
+        },
+        headers: { // 设置请求时的header信息
+          host: 'http://www.dell-lee.com',
+          cookie: 'this is'
+        }
+      }
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
--- serverProxy/src/index.js
+++ serverProxy/src/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import axios from 'axios';
 
 class App extends Component {
+  // 组件挂载之后调用此函数
+  componentDidMount () {
+    axios.get('/react/api/header.json').then(function (res) {
+      console.log(res);
+    })
+  }
   render () {
     return <div>Hello World</div>
   }
