584738eff826128112be46c219b63c2baf3b193f
recoveryMonster <fengsongting@gmail.com>
2020-12-13 16:54:54 +0800

    add @bable/parser to parse sourcecode to ast

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -1,7 +1,14 @@
 // 模拟模块打包器
+const fs = require('fs');
+const babelParser = require('@babel/parser');
 
 const moduleAnalysers = (filename) => {
-
+  const fileInfo = fs.readFileSync(filename, 'utf-8');
+  // 将代码解析为AST语法树
+  const astInfo = babelParser.parse(fileInfo, {
+    sourceType: 'module'
+  });
+  console.log(astInfo.program.body)
 }
 
 moduleAnalysers('./src/index.js');
